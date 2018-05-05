516b36e5474f557e7881cfead8a901a00f3fe268
pgieschen <philipp.gieschen@zalando.ie>
2018-05-05 16:47:29 +0100

    added menu animation

---
+++ app/app/static/menu.js
@@ -0,0 +1,7 @@
+$(document).ready(function ($) {
+    $('#menu > ul.nav li a').click(function(e) {
+        var $this = $(this);
+        $this.parent().siblings().removeClass('active').end().addClass('active');
+        e.preventDefault();
+    });
+});
